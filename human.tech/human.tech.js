c6b53d82cf71762a34c57d6358caec992cb84723
David He <david.he@magic.link>
2026-01-16 01:25:26 -0500

    integrate proof of clean hands

--- human.tech/human.tech.js
+++ human.tech/human.tech.js
@@ -28,13 +28,29 @@ async function getModelsScore(address) {
     return body;
 }
 
+async function getProofOfCleanHands(address) {
+    const request = {
+      url: `https://mainnet-rpc.sign.global/api/scan/addresses/${address}/attestations`,
+      method: "GET",
+      headers: [["Content-Type", "application/json"]],
+    };
+    const response = await httpFetch(request);
+    const body = JSON.parse(new TextDecoder().decode(new Uint8Array(response.body)));
+    return body.data;
+}
+
 export async function run(wasm_args) {
     const wasmArgs = JSON.parse(wasm_args);
     const { address } = wasmArgs;
     try {
         const stampsScore = await getStampsScore(address);
         const modelsScore = await getModelsScore(address);
-        return JSON.stringify({ stamps_score: stampsScore, models_score: modelsScore });
+        const proofOfCleanHands = await getProofOfCleanHands(address);
+        return JSON.stringify({ 
+            stamps_score: stampsScore,
+            models_score: modelsScore,
+            proof_of_clean_hands: proofOfCleanHands
+        });
     } catch (err) {
         return JSON.stringify({ error: err.message || String(err) });
     }
